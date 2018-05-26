ff474df354404d7d26788f85db94c923cb4c1dc3
EAST <417034606@qq.com>
2018-05-27 00:52:04 +0800

    v0.0.3

--- Public/create_html.py
+++ Public/create_html.py
@@ -12,9 +12,35 @@ def title(titles):
 	<head>
 		<meta charset="UTF-8">
 		<title>%s</title>
+		<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<style type="text/css">
-			td{ width:40px; height:50px;}
+
+			td{ 
+				width:80px; 
+			}
+			
+			.ellps {
+				width:200px;
+/*				text-overflow: ellipsis; //超出部分用....代替  
+				overflow: hidden; //超出隐藏  
+				border: 1px solid red;*/
+				overflow: hidden;
+				text-overflow: ellipsis;
+				display: -webkit-box;
+				-webkit-line-clamp: 4;
+				-webkit-box-orient: vertical;
+			}
+			.modal-dialog {
+			    width: 800px;
+			}
+			.table{
+			width: 75%%;
+			margin:0 auto;
+			}
 		</style>
+
+		<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 	''' % (titles)
@@ -34,8 +60,8 @@ def shouye(starttime, endtime, passge, fail, excepthions, weizhicuowu):
 		<p><strong>结果:</strong>
 			<span >Pass: <strong >%s</strong>
 			Fail: <strong >%s</strong>
-			       exception: <strong >%s</strong> 
-			       weizhicuowu : <strong >%s</strong></span></p>                  
+			       Exception: <strong >%s</strong> 
+			       Unknown_Error : <strong >%s</strong></span></p>                  
 			    <p ><strong>测试详情如下</strong></p>  </div> ''' % (
     starttime, endtime, (endtime - starttime), passge, fail, excepthions, weizhicuowu)
     return beijing
@@ -43,7 +69,7 @@ def shouye(starttime, endtime, passge, fail, excepthions, weizhicuowu):
 
 shanghai = '''
         <p>&nbsp;</p>
-        <table border='2'cellspacing='1' cellpadding='1' width='1100'align="center" >
+        <table class="table table-bordered" >
 		<tr >
             <td ><strong>用例ID&nbsp;</strong></td>
             <td><strong>用例名字</strong></td>
@@ -54,7 +80,6 @@ shanghai = '''
             <td><strong>预期</strong></td>
             <td><strong>实际返回</strong></td>  
             <td><strong>结果</strong></td>
-            <td><button>anniu</button></td>
         </tr>
     '''
 
@@ -76,15 +101,17 @@ def ceshixiangqing(id, name, key, coneent, url, meth, yuqi, json, relust):
         <tr width='100'>
             <td>%s</td>
             <td>%s</td>
-
-            <td>%s</td>
+            <td class="warp">%s</td>
             <td>%s
            </td>
             <td>%s</td>
             <td>%s</td>
             <td>%s</td>
-            <td>%s</td>
-            %s
+             <td>
+            	<div class='ellps'>%s</div>
+            	<a class="btn btn-link a_null">show</a>
+            	</td>
+            	%s
         </tr>
 
     ''' % (id, name, key, coneent, url, meth, yuqi, json, passfail(relust))
@@ -93,7 +120,47 @@ def ceshixiangqing(id, name, key, coneent, url, meth, yuqi, json, relust):
 
 weibu = '''
 	</table>
+	<!-- 模态框（Modal） -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">实际返回</h4>
+            </div>
+            <div class="modal-body">在这里添加一些文本</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal -->
+</div>
     </body>
+    <script type="text/javascript">
+    
+    	function stringToNum(str, num) {
+    		var len = Math.floor(str.length/num)
+    		var res = ''
+    		for(var i=0; i<num; i++){
+    			res = res + str.substr(len*i,len) + '\\r\\n'
+    		}
+    		res = res + str.substring(len*num, str.length)
+    		return res
+    	}
+    	var cxt = $(".warp");
+    	for(var t=0;t<cxt.length;t++)
+    	cxt.get(t).innerText = stringToNum(cxt.get(t).innerText,6)
+    	//换行
+    	
+    	$('.a_null').click(function(){
+    		var text = $(this).prev().text()
+    		console.log(text)
+    		$('.modal-body').text(text)
+    		$('#myModal').modal('show')
+    	})
+    	//显示模态框
+    	
+    </script>
     </html>'''
 
 
